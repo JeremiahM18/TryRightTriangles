f35e22f150a8d52fd37a61f55ea6139ead8d316e
Jeremiah <jermcdon18@gmail.com>
2026-02-07 18:46:47 -0600

    Add PointStore interface for abstract point storage

---
+++ src/com/tryright/PointStore.java
@@ -0,0 +1,33 @@
+package com.tryright;
+
+/**
+ * Point storage interface
+ * @version 1.0
+ */
+
+public interface PointStore {
+    /**
+     * Get X value at given index
+     *
+     * @param idx index from which to fetch values
+     * @return x value at given index
+     */
+    int getX (int idx);
+    /**
+     * Get Y value at given index
+     *
+     * @param idx index from which to fetch values
+     * @return Y value at given index
+     */
+    int getY (int idx);
+    /**
+     * Get number of points in store
+     *
+     * @return number of points in store
+     */
+    int numPoints();
+    /**
+     * close point store
+     */
+    void close();
+}
