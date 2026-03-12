7f984586d6eba7ee4eb1a086a22b893d452abcd7
Jeremiah <jermcdon18@gmail.com>
2026-03-12 12:44:35 -0500

    Update PointStore interface to v1.1 and add provided test files
    - Replaced the previous PointStore interface with v1.1
    - Added test classes to the project for validation

--- src/com/pizza/TestDriver.java
+++ src/com/pizza/TestDriver.java
@@ -96,8 +96,8 @@ public final class TestDriver {
         final int maxSlices = 20;
 
         // REQUIRED: single line to choose implementation:
-        selected = new BuffetMonitor(maxSlices);
-        // selected = new BuffetSemaphore(maxSlices);
+        // selected = new BuffetMonitor(maxSlices);
+        selected = new BuffetSemaphore(maxSlices);
         // selected = new BuffetLock(maxSlices);
 
         log("=== TestDriver starting ===");
--- src/com/tryright/PointStore.java
+++ src/com/tryright/PointStore.java
@@ -1,41 +1,37 @@
-/****************************************
- *
- * Author: Jeremiah McDonald
- * Assignment: Program 3
- * Class: CSC-4180 Operating Systems
- *
- ***************************************/
-
 package com.tryright;
 
 /**
  * Point storage interface
- * @version 1.0
+ * @version 1.1
  */
-
 public interface PointStore {
-    /**
-     * Get X value at given index
-     *
-     * @param idx index from which to fetch values
-     * @return x value at given index
-     */
-    int getX (int idx);
-    /**
-     * Get Y value at given index
-     *
-     * @param idx index from which to fetch values
-     * @return Y value at given index
-     */
-    int getY (int idx);
-    /**
-     * Get number of points in store
-     *
-     * @return number of points in store
-     */
-    int numPoints();
-    /**
-     * close point store
-     */
-    void close();
+  /**
+   * Get X value at given index
+   *
+   * @param idx index from which to fetch values
+   * @return X value at given index
+   * @throws IndexOutOfBoundsException if idx < 0 or idx >= numPoints
+   */
+  int getX(int idx);
+
+  /**
+   * Get Y value at given index
+   *
+   * @param idx index from which to fetch values
+   * @return Y value at given index
+   * @throws IndexOutOfBoundsException if idx < 0 or idx >= numPoints
+   */
+  int getY(int idx);
+
+  /**
+   * Get number of points in store
+   *
+   * @return number of points in store
+   */
+  int numPoints();
+
+  /**
+   * Close point store
+   */
+  void close();
 }
